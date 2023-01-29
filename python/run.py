91d9ab56056eefee7054727ed85618b6b425a00f
Kat Iliad <121892621+katiliad@users.noreply.github.com>
2023-01-29 22:52:56 +0200

    Remove unecessary if main = name

--- python/run.py
+++ python/run.py
@@ -15,8 +15,8 @@ def writeRawResponseToFile(response):
 
 def downloadData(from_field, to_field, date):
     amadeus = Client(
-        client_id='xx',
-        client_secret='xx'
+        client_id='X',
+        client_secret='X'
     )
 
     try:
@@ -28,53 +28,51 @@ def downloadData(from_field, to_field, date):
         raise error
 
 def runSparkOnRawFile():
+    spark = SparkSession.builder.master("local[*]").appName("Read corrupted json").getOrCreate()
 
-    if __name__ == "__main__":
-        spark = SparkSession.builder.master("local[*]").appName("Read corrupted json").getOrCreate()
+    dfFromTxt=spark.read.text(raw_filename)
+    dfFromTxt.show(truncate=False)
 
-        dfFromTxt=spark.read.text(raw_filename)
-        dfFromTxt.show(truncate=False)
+    from pyspark.sql.functions import regexp_replace
+    dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', 'False', '"FALSE"'))
+    dfFromTxt.count()
 
-        from pyspark.sql.functions import regexp_replace
-        dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', 'False', '"FALSE"'))
-        dfFromTxt.count()
+    dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', '\},\ \{', '\}__SPLIT_POINT__\{'))
+    dfFromTxt.show(truncate=False)
 
-        dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', '\},\ \{', '\}__SPLIT_POINT__\{'))
-        dfFromTxt.show(truncate=False)
+    from pyspark.sql.types import StructType,StructField,IntegerType,StringType,DoubleType,BooleanType
 
-        from pyspark.sql.types import StructType,StructField,IntegerType,StringType,DoubleType,BooleanType
+    schema = StructType([
+        StructField("Brand",StringType(),True),
+        StructField("Days", IntegerType(), True),
+        StructField("Model", StringType(), True),
+        StructField("Rate", DoubleType(), True),
+        StructField("Available", StringType(), True)
+    ])
 
-        schema = StructType([
-            StructField("Brand",StringType(),True),
-            StructField("Days", IntegerType(), True),
-            StructField("Model", StringType(), True),
-            StructField("Rate", DoubleType(), True),
-            StructField("Available", StringType(), True)
-        ])
-
-        from pyspark.sql.functions import col,from_json
-        dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
-        dfJSON.show()
+    from pyspark.sql.functions import col,from_json
+    dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
+    dfJSON.show()
 
 
-        from pyspark.sql.functions import split, explode
-        dfFromTxt = dfFromTxt.withColumn('value',explode(split('value','__SPLIT_POINT__')))
-        dfFromTxt.count()
+    from pyspark.sql.functions import split, explode
+    dfFromTxt = dfFromTxt.withColumn('value',explode(split('value','__SPLIT_POINT__')))
+    dfFromTxt.count()
 
-        dfFromTxt.show(truncate=False)
-
-
-        dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
-        dfJSON.show()
+    dfFromTxt.show(truncate=False)
 
 
-        dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', '\[', '')).withColumn('value', regexp_replace('value', '\]', ''))
-        dfFromTxt.show(truncate=False)
+    dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
+    dfJSON.show()
 
-        dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
-        dfJSON.show()
 
-        dfJSON.write.format('json').mode('overwrite').save("../output/"+processed_filename)
+    dfFromTxt = dfFromTxt.withColumn('value', regexp_replace('value', '\[', '')).withColumn('value', regexp_replace('value', '\]', ''))
+    dfFromTxt.show(truncate=False)
+
+    dfJSON = dfFromTxt.withColumn("jsonData",from_json(col("value"),schema)).select("jsonData.*").dropna()
+    dfJSON.show()
+
+    dfJSON.write.format('json').mode('overwrite').save("../output/"+processed_filename)
 
 def main():
     from_field = sys.argv[1]
