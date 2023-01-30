c9b231d9fb13dd5eabe27cfe2bbefa9d1117763d
Kat Iliad <121892621+katiliad@users.noreply.github.com>
2023-01-31 00:22:31 +0200

    Removed unecessary dataframe show in Spark

--- spark/streaming_spark.py
+++ spark/streaming_spark.py
@@ -11,47 +11,48 @@ from pyspark.sql.functions import explode, min, max, first, col, count, expr, co
 raw_filename = "reponse_raw.json"
 processed_filename = "output_jsn.json"
 rawDF=spark.read.json(raw_filename, multiLine="true")
-rawDF.printSchema()
+#rawDF.printSchema()
 
 sampleDF = rawDF.withColumnRenamed("id", "unique_travel_id")
 
 priceDF = sampleDF.select("unique_travel_id", concat("price.grandTotal","price.currency").alias("Price"))
-priceDF.show()
+#priceDF.show()
 
 
 batDF = sampleDF.select("unique_travel_id", "itineraries")
-batDF.printSchema()
-batDF.show(1, False)
+#batDF.printSchema()
+#batDF.show(1, False)
 bat2DF = batDF.select("unique_travel_id", explode("itineraries").alias("new_itineraries"))
-bat2DF.printSchema()
-bat2DF.show()
+#bat2DF.printSchema()
+#bat2DF.show()
 
 bat3DF =  bat2DF.select("unique_travel_id", "new_itineraries.segments")
-bat3DF.printSchema()
-bat3DF.show()
+#bat3DF.printSchema()
+#bat3DF.show()
 
 bat4DF = bat3DF.select("unique_travel_id", explode("segments").alias("exploded_segments"))
-bat4DF.printSchema()
-bat4DF.show()
+#bat4DF.printSchema()
+#bat4DF.show()
 
 bat5DF = bat4DF.select("unique_travel_id", col("exploded_segments.operating.carrierCode").alias("IATA Code"), col("exploded_segments.departure.at").alias("departure"), col("exploded_segments.arrival.at").alias("arrival"))
-bat5DF.show()
+#bat5DF.show()
 
 bat6DF = bat5DF.groupBy("unique_travel_id").agg(
-    min("departure").alias("departure"),
-    max("arrival").alias("arrival"),
+    min("departure").alias("Departure"),
+    max("arrival").alias("Arrival"),
     first("IATA Code").alias("IATA Code"),
 	count('*').alias('Stops')
 ).dropDuplicates(["unique_travel_id"])
 
 bat6DF = bat6DF.withColumn("Stops", expr("Stops - 1"))
-bat6DF.show()
+#bat6DF.show()
 
 result_df = bat6DF.join(priceDF, on="unique_travel_id", how="inner")
-result_df.show(150)
+#result_df.show(150)
 
 result_without_id_df = result_df.drop(col("unique_travel_id"))
 result_without_id_df.show(150)
+
 #Get the size of a column to create anotehr column
 #bat3DF.withColumn("segments_length",size(col("segments"))).show(False)
 #bat3DF.selectExpr("*", "size(array_distinct(flatten(transform(segments, (v, i) -> array(v._1, v._2))))) as count").show(False)
